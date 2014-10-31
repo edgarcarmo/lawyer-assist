aa6a674351778732319b3539893a7021c655e256
edgarcarmo <edgarcarmo@hotmail.com>
2014-10-31 17:54:20 -0200

    Inclusão da tela de advogados (ainda sem implementar Angularjs)

---
+++ includes/modal/lawyer_add.php
@@ -0,0 +1,154 @@
+<div class="modal fade"  id="lawyer_add" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Fechar</span></button>
+				<h4 class="modal-title">Advogado</h4>
+			</div>
+			<div class="modal-body">
+				<form action="#" role="form">
+					<div class="container-fluid">
+						<div class="row">
+							<div class="col-md-8">
+								<div class="form-group">
+									<label for="oab">OAB</label>
+									<input type="text" id="oab" class="form-control" placeholder="OAB" />
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="form-group">
+									<label for="oabuf">Estado OAB</label>
+									<select name="oabuf" id="oabuf" class="form-control">
+										<option value="estado" selected="selected">Selecione o Estado</option> 
+										<option value="ac">Acre</option> 
+										<option value="al">Alagoas</option> 
+										<option value="am">Amazonas</option> 
+										<option value="ap">Amapá</option> 
+										<option value="ba">Bahia</option> 
+										<option value="ce">Ceará</option> 
+										<option value="df">Distrito Federal</option> 
+										<option value="es">Espírito Santo</option> 
+										<option value="go">Goiás</option> 
+										<option value="ma">Maranhão</option> 
+										<option value="mt">Mato Grosso</option> 
+										<option value="ms">Mato Grosso do Sul</option> 
+										<option value="mg">Minas Gerais</option> 
+										<option value="pa">Pará</option> 
+										<option value="pb">Paraíba</option> 
+										<option value="pr">Paraná</option> 
+										<option value="pe">Pernambuco</option> 
+										<option value="pi">Piauí</option> 
+										<option value="rj">Rio de Janeiro</option> 
+										<option value="rn">Rio Grande do Norte</option> 
+										<option value="ro">Rondônia</option> 
+										<option value="rs">Rio Grande do Sul</option> 
+										<option value="rr">Roraima</option> 
+										<option value="sc">Santa Catarina</option> 
+										<option value="se">Sergipe</option> 
+										<option value="sp">São Paulo</option> 
+										<option value="to">Tocantins</option>
+									</select>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-4">
+								<div class="form-group">
+									<label for="cpf">CPF</label>
+									<input type="text" id="cpf" class="form-control" placeholder="CPF" />
+								</div>
+							</div>
+							<div class="col-md-8">
+								<div class="form-group">
+									<label for="name">Nome</label>
+									<input type="text" id="name" class="form-control" placeholder="Nome completo" />
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="phone">Telefone</label>
+									<input type="text" id="phone" class="form-control" placeholder="Telefone" />
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="cellphone">Celular</label>
+									<input type="text" id="cellphone" class="form-control" placeholder="Celular" />
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<div class="form-group">
+									<label for="email">E-mail</label>
+									<input type="text" id="email" class="form-control" placeholder="E-mail" />
+								</div>
+							</div>
+						</div>
+						<!--
+						<div class="row">
+							<div class="col-md-12">
+								&nbsp;
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-4">
+								<div class="form-group">
+									<label for="cep">CEP</label>
+									<input type="text" id="cep" class="form-control" placeholder="CEP" />
+								</div>						
+							</div>
+							<div class="col-md-8">
+								<div class="form-group">
+									<label for="address">Endereço</label>
+									<input type="text" id="address" class="form-control" placeholder="Endereço" />
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-4">
+								<div class="form-group">
+									<label for="number">Número</label>
+									<input type="text" id="number" class="form-control" placeholder="Número" />
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="form-group">
+									<label for="complement">Complemento</label>
+									<input type="text" id="complement" class="form-control" placeholder="Complemento" />
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="form-group">
+									<label for="neighborhood">Bairro</label>
+									<input type="text" id="neighborhood" class="form-control" placeholder="Bairro" />
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-8">
+								<div class="form-group">
+									<label for="city">Cidade</label>
+									<input type="text" id="city" class="form-control" placeholder="Cidade" />
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="form-group">
+									<label for="state">Estado</label>
+									<input type="text" id="state" class="form-control" placeholder="Estado" />
+								</div>
+							</div>
+						</div>
+						-->
+					</div>
+				</form>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+				<button type="button" class="btn btn-success">Salvar</button>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
